dc1c3546c3a0a513d8745268a49f93e136b00a5b
vyshor <vyshornehc@gmail.com>
2023-08-02 02:25:27 +0800

    Add constant

---
+++ src/main/java/com/service/spring/jedi/constant/JediConstants.java
@@ -0,0 +1,8 @@
+package com.service.spring.jedi.constant;
+
+public class JediConstants {
+    public static final String DARTH_VADER_PERSON_NAME = "Darth Vader";
+    public static final String LEIA_ORGANA_PERSON_NAME = "Leia Organa";
+    public static final String ALDERAAN_PLANET_NAME = "Alderaan";
+    public static final String DEATH_STAR_STARSHIP_NAME = "Death Star";
+}
