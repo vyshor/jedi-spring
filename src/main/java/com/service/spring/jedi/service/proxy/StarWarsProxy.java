abf5ce1bffc5f6716f151a2b4ca08a4b137926df
vyshor <vyshornehc@gmail.com>
2023-08-02 01:29:45 +0800

    Add StarWars proxy

---
+++ src/main/java/com/service/spring/jedi/service/proxy/StarWarsProxy.java
@@ -0,0 +1,35 @@
+package com.service.spring.jedi.service.proxy;
+
+import com.service.spring.jedi.dto.PersonProxyDto;
+import com.service.spring.jedi.dto.PlanetProxyDto;
+import com.service.spring.jedi.dto.StarshipProxyDto;
+import com.service.spring.jedi.response.SearchResponseBody;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+/**
+ * @author vyshor
+ */
+@FeignClient(name = "swapi.dev/api/")
+public interface StarWarsProxy {
+
+    @RequestMapping(value = "/starships", method = RequestMethod.GET)
+    SearchResponseBody<List<StarshipProxyDto>> getStarshipProxyDtosByName(@RequestParam("search") String name);
+
+    @RequestMapping(value = "/planets", method = RequestMethod.GET)
+    SearchResponseBody<List<PlanetProxyDto>> getPlanetProxyDtosByName(@RequestParam("search") String name);
+
+    @RequestMapping(value = "/people", method = RequestMethod.GET)
+    SearchResponseBody<List<PersonProxyDto>> getPersonProxyDtosByName(@RequestParam("search") String name);
+
+    @RequestMapping(value = "/people/{id}", method = RequestMethod.GET)
+    PersonProxyDto getPersonProxyDtoById(@PathVariable("id") String id);
+
+    @RequestMapping(value = "/starships/{id}", method = RequestMethod.GET)
+    StarshipProxyDto getStarshipProxyDtoById(@PathVariable("id") String id);
+}
